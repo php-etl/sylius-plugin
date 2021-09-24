8b0b04fa9448484b1672bfa02eb4aadb16333369
Grégory Planchat <gregory@kiboko.fr>
2021-09-24 12:00:24 +0200

    Updated php-etl/configurator-contracts to version 0.4 in order to help automatic plugin loading in php-etl/satellite

--- src/Configuration.php
+++ src/Configuration.php
@@ -2,10 +2,10 @@
 
 namespace Kiboko\Plugin\Sylius;
 
+use Kiboko\Contract\Configurator\PluginConfigurationInterface;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
-use Symfony\Component\Config\Definition\ConfigurationInterface;
 
-final class Configuration implements ConfigurationInterface
+final class Configuration implements PluginConfigurationInterface
 {
     public function getConfigTreeBuilder()
     {
